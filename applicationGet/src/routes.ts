f3dc10816cb5ac0d57f25dadd9f9ab94540982fd
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-12 16:39:29 +0330

    adf

--- applicationGet/src/routes.ts
+++ applicationGet/src/routes.ts
@@ -222,13 +222,13 @@ export const Routes = [
     action: "getGoldPrice",
     middlwares: [authenticate]
 },
-// {
-//     method: "get",
-//     route: "/goldPrice/:date",
-//     controller: GoldPriceController,
-//     action: "getGoldPriceForDate",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/goldPrice/:date",
+    controller: GoldPriceController,
+    action: "getGoldPriceForDate",
+    middlwares: [adminMiddleware]
+},
 // {
 //     method: "post",
 //     route: "/deposit",
@@ -348,19 +348,22 @@ export const Routes = [
 //     controller: inPersonController,
 //     action: "creatSellTransActions",         /**its for creating sell transAction in inperson purchase */
 //     middlwares: [adminMiddleware,creatSellTransAction]
-// },{
-//     method: "get",
-//     route: "/inperson/sell/all/:status",
-//     controller: inPersonController,
-//     action: "getAllSellPending",         /**its for get all pending inperson purchase */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/inperson/buy/all/:status",
-//     controller: inPersonController,
-//     action: "getAllBuyPending",         /**its for get all pending inperson purchase */
-//     middlwares: [adminMiddleware]
-// },{
+// },
+{
+    method: "get",
+    route: "/inperson/sell/all/:status",
+    controller: inPersonController,
+    action: "getAllSellPending",         /**its for get all pending inperson purchase */
+    middlwares: [adminMiddleware]
+},
+{
+    method: "get",
+    route: "/inperson/buy/all/:status",
+    controller: inPersonController,
+    action: "getAllBuyPending",         /**its for get all pending inperson purchase */
+    middlwares: [adminMiddleware]
+},
+// {
 //     method: "post",
 //     route: "/inperson/transaction/changestatus",
 //     controller: inPersonController,
@@ -378,110 +381,118 @@ export const Routes = [
 // /**
 //  * these routes is for admin pannel . . .
 //  */
-// {
-//     method: "get",
-//     route: "/admin/users/all",
-//     controller: adminController,
-//     action: "getAllUsers",              /**its for get all users by admin */
-//     middlwares: [adminMiddleware]
-// }, {
-//     method: "get",
-//     route: "/admin/wallet/all",
-//     controller: adminController,
-//     action: "getAllWallet",             /**its for get all wallet by admin */
-//     middlwares: [adminMiddleware]
-// }, {
-//     method: "get",
-//     route: "/admin/transactions/all",
-//     controller: adminController,
-//     action: "getAllTransActions",        /**its for get all transActions by admin */
-//     middlwares: [adminMiddleware]
-// }, {                                  
+{
+    method: "get",
+    route: "/admin/users/all",
+    controller: adminController,
+    action: "getAllUsers",              /**its for get all users by admin */
+    middlwares: [adminMiddleware]
+}, 
+{
+    method: "get",
+    route: "/admin/wallet/all",
+    controller: adminController,
+    action: "getAllWallet",             /**its for get all wallet by admin */
+    middlwares: [adminMiddleware]
+},
+ {
+    method: "get",
+    route: "/admin/transactions/all",
+    controller: adminController,
+    action: "getAllTransActions",        /**its for get all transActions by admin */
+    middlwares: [adminMiddleware]
+}, 
+// {                                  
 //     method: "put",
 //     route: "/admin/transactions/:transactionId",
 //     controller: adminController,
 //     action: "handledVerify",            //**its for verifying the transaction by admin */
 //     middlwares: [adminMiddleware , verifyTransActinon]
-// }, {                         
-//     method: "get",
-//     route: "/admin/deposit/pending",
-//     controller: adminController,
-//     action: "getAllPendingsDeposit",       /**its for getting all pendings deposit with admin */
-//     middlwares: [adminMiddleware]
-// },{                         
-//     method: "get",
-//     route: "/admin/deposit/succeed",
-//     controller: adminController,
-//     action: "getAllSucceedDeposit",       /**its for getting all succeed deposit with admin */
-//     middlwares: [adminMiddleware]
-// },{                          
+// }, 
+{                         
+    method: "get",
+    route: "/admin/deposit/pending",
+    controller: adminController,
+    action: "getAllPendingsDeposit",       /**its for getting all pendings deposit with admin */
+    middlwares: [adminMiddleware]
+},
+{                         
+    method: "get",
+    route: "/admin/deposit/succeed",
+    controller: adminController,
+    action: "getAllSucceedDeposit",       /**its for getting all succeed deposit with admin */
+    middlwares: [adminMiddleware]
+},
+// {                          
 //     method: "put",
 //     route: "/admin/deposit/:depositId",
 //     controller: adminController,
 //     action: "handleVerifyDeposit",        /**its for verifying the deposit by admin */
 //     middlwares: [adminMiddleware , verifyTransActinon]
 // },
-// {
-//     method: "get",
-//     route: "/admin/home",
-//     controller: adminController,
-//     action: "homeData",                  /**its for getting home data */
-//     middlwares: [adminMiddleware]
-// }, {
-//     method: "get",
-//     route: "/admin/withdraw/pending",
-//     controller: adminController,
-//     action: "getAllWithdrawals",         /**its for getting all pendings withdrawal requests */
-//     middlwares: [adminMiddleware]
-// }, {
+{
+    method: "get",
+    route: "/admin/home",
+    controller: adminController,
+    action: "homeData",                  /**its for getting home data */
+    middlwares: [adminMiddleware]
+}, {
+    method: "get",
+    route: "/admin/withdraw/pending",
+    controller: adminController,
+    action: "getAllWithdrawals",         /**its for getting all pendings withdrawal requests */
+    middlwares: [adminMiddleware]
+},
+//  {
 //     method: "put",
 //     route: "/admin/withdraw/:transActionId",
 //     controller: adminController,
 //     action: "approveWithdrawal",         /**its for approve withdraw by admin after pay to user */
 //     middlwares: [adminMiddleware , approveWithdrawalDTO]
-// }, {
-//     method: "get",
-//     route: "/admin/user/:userId",
-//     controller: adminController,
-//     action: "getUserInfo",              /**its for get user ifo by admin */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/withdraw/succeed",
-//     controller: adminController,
-//     action: "getSucceedWithdrawal",      /**its for getting succeeded withdrawal  */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/deposit/failed",
-//     controller: adminController,
-//     action: "allFailedDeposit",           /**its for ggetting all failed deposit  */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/transactions/buy",
-//     controller: adminController,
-//     action: "getBuyTransAction",           /**its for getting all buy transactions  */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/transactions/sell",
-//     controller: adminController,
-//     action: "getSellTransAction",           /**its for getting all sell transactions  */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/transactions/init",
-//     controller: adminController,
-//     action: "getUserForInitTransactions",           /**its for getting all sell transactions  */
-//     middlwares: [adminMiddleware]
-// },{
-//     method: "get",
-//     route: "/admin/transactions/:userId",
-//     controller: adminController,
-//     action: "getUserForInitTransactions",           /**its for getting all sell transactions  */
-//     middlwares: [adminMiddleware]
 // },
+ {
+    method: "get",
+    route: "/admin/user/:userId",
+    controller: adminController,
+    action: "getUserInfo",              /**its for get user ifo by admin */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/withdraw/succeed",
+    controller: adminController,
+    action: "getSucceedWithdrawal",      /**its for getting succeeded withdrawal  */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/deposit/failed",
+    controller: adminController,
+    action: "allFailedDeposit",           /**its for ggetting all failed deposit  */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/transactions/buy",
+    controller: adminController,
+    action: "getBuyTransAction",           /**its for getting all buy transactions  */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/transactions/sell",
+    controller: adminController,
+    action: "getSellTransAction",           /**its for getting all sell transactions  */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/transactions/init",
+    controller: adminController,
+    action: "getUserForInitTransactions",           /**its for getting all sell transactions  */
+    middlwares: [adminMiddleware]
+},{
+    method: "get",
+    route: "/admin/transactions/:userId",
+    controller: adminController,
+    action: "getUserForInitTransactions",           /**its for getting all sell transactions  */
+    middlwares: [adminMiddleware]
+},
 
 
 
@@ -517,13 +528,13 @@ export const Routes = [
 /**
  * this rout is for home page charts
  */
-// {
-//     method: "get",
-//     route: "/home/charts",
-//     controller: adminController,
-//     action: "homeData",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/home/charts",
+    controller: adminController,
+    action: "homeData",
+    middlwares: [adminMiddleware]
+},
 
 
 
@@ -553,13 +564,13 @@ export const Routes = [
 //     action: "rejectPhoneBuyInvocie",
 //     middlwares: [adminMiddleware]
 // },
-// {
-//     method: "get",
-//     route: "/call",
-//     controller: PhoneInvoiceController,
-//     action: "getAllBuyPhoneInvoice",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/call",
+    controller: PhoneInvoiceController,
+    action: "getAllBuyPhoneInvoice",
+    middlwares: [adminMiddleware]
+},
 // {
 //     method: "put",
 //     route: "/call/update/:id",
@@ -567,13 +578,13 @@ export const Routes = [
 //     action: "updatePhoneInvoice",
 //     middlwares: [adminMiddleware]
 // },
-// {
-//     method: "get",
-//     route: "/call/status/:status",
-//     controller: PhoneInvoiceController,
-//     action: "getBuyPhoneInvoice",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/call/status/:status",
+    controller: PhoneInvoiceController,
+    action: "getBuyPhoneInvoice",
+    middlwares: [adminMiddleware]
+},
 {
     method: "get",
     route: "/call/user",
@@ -596,20 +607,20 @@ export const Routes = [
 //     action: "setPayment",
 //     middlwares: [adminMiddleware]
 // },
-// {
-//     method: "get",
-//     route: "/inperson/convert/all",
-//     controller: invoiceConvertorController,
-//     action: "getAllConvertsInvoice",
-//     middlwares: [adminMiddleware]
-// },
-// {
-//     method: "get",
-//     route: "/inperson/convert/:id",
-//     controller: invoiceConvertorController,
-//     action: "getSpecificConvertInvoice",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/inperson/convert/all",
+    controller: invoiceConvertorController,
+    action: "getAllConvertsInvoice",
+    middlwares: [adminMiddleware]
+},
+{
+    method: "get",
+    route: "/inperson/convert/:id",
+    controller: invoiceConvertorController,
+    action: "getSpecificConvertInvoice",
+    middlwares: [adminMiddleware]
+},
 
 /**
  * sell call
@@ -623,13 +634,13 @@ export const Routes = [
 //     middlwares: [adminMiddleware]
 // },
 
-// {
-//     method: "get",
-//     route: "/selllist",
-//     controller: PhoneInvoiceController,
-//     action: "getAllSellPhoneInvoice",
-//     middlwares: [adminMiddleware]
-// },
+{
+    method: "get",
+    route: "/selllist",
+    controller: PhoneInvoiceController,
+    action: "getAllSellPhoneInvoice",
+    middlwares: [adminMiddleware]
+},
 {
     method: "get",
     route: "/callsell/user",
@@ -637,13 +648,13 @@ export const Routes = [
     action: "getAllSellPhoneTransactionForUser",
     middlwares: [authenticate]
 },
-// {
-//     method: "get",
-//     route: "/callsell/status/:status",
-//     controller: PhoneInvoiceController,
-//     action: "getSellPhoneInvoice",
-//     middlwares: [adminMiddleware]
-// }
+{
+    method: "get",
+    route: "/callsell/status/:status",
+    controller: PhoneInvoiceController,
+    action: "getSellPhoneInvoice",
+    middlwares: [adminMiddleware]
+}
 
 /**
  * this route is just for test
