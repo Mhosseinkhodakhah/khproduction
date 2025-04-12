bba2dae150a35060f15f711aeb2bf8f29400b49c
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-12 15:59:59 +0330

    3003

--- applicationGet/src/index.ts
+++ applicationGet/src/index.ts
@@ -104,6 +104,6 @@ AppDataSource.initialize().then(async () => {
         console.log('error occured . . .', error)
     })
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+    console.log("Express server has started on port 3003. Open http://localhost:3003/users to see results")
 
 }).catch(error => console.log(error))
