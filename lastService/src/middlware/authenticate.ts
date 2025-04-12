5ef6242bb3033a15efc103754bce3d23b9615cb5
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-12 15:00:40 +0330

    add

--- lastService/src/middlware/authenticate.ts
+++ lastService/src/middlware/authenticate.ts
@@ -11,7 +11,7 @@ export  function authenticate (req, res, next) {
       console.log('check the blacklist>>>>>' , blackList.checker(token))
       monitor.error.push(`تلاش برای ورود با توکن غیر مجاز`)
       res.status(401).json({
-        error: "Unauthorized request"
+        error: "token is in the blackList"
       });
     }
     const decodedToken = jwt.verify(token, '0258f34c736db4a7603d2e6d8b45ef3ef742af9256f8b821f9a51b3c54b11a72');    
