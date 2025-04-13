b1a8935b05ac8d5623a9424b29861d2ae79d7d74
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-13 11:20:05 +0330

    adf

--- lastService/src/controller/BankAccountController.ts
+++ lastService/src/controller/BankAccountController.ts
@@ -115,15 +115,15 @@ export class BankAccountController {
                 birthDate : owner.birthDate
                 }
                 console.log(info)
-                // let isMatch = await this.checkCard.checkCardNuber(info)
-                let isMatch = await  this.shahkarController.checkMatchPhoneNumberAndCartNumber(info)
-                console.log('its returned data>>>' , isMatch)
+                let isMatch = await this.checkCard.checkCardNuber(info)
+                // let isMatch = await  this.shahkarController.checkMatchPhoneNumberAndCartNumber(info)
+                // console.log('its returned data>>>' , isMatch)
                 if (isMatch == 500){
                     return response.status(500).json({msg : 'سیستم ثبت کارت بانکی موقتا در دسترس نمیباشد.لطفا دقایقی دیگر مجددا تلاش کنید'})
                 }
-                if (typeof(isMatch) === undefined){
-                    return response.status(500).json({msg : 'سیستم ثبت کارت بانکی موقتا در دسترس نمیباشد.لطفا دقایقی دیگر مجددا تلاش کنید'})
-                }
+                // if (typeof(isMatch) === undefined){
+                //     return response.status(500).json({msg : 'سیستم ثبت کارت بانکی موقتا در دسترس نمیباشد.لطفا دقایقی دیگر مجددا تلاش کنید'})
+                // }
                 if (isMatch == false){
                     monitor.addStatus({
                         scope : 'bank account controller',
@@ -132,7 +132,6 @@ export class BankAccountController {
                     })
                     response.status(400).json({ msg: "کارت نامعتبر است" });
                 }
-
                 bankAccount.isVerified = isMatch;
                 
                 if (isMatch) {                
