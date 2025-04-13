7a317ee8f85d8cc5f083f62fe9941c068265647a
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-13 16:19:37 +0330

    asdf

--- lastService2/src/controller/UserController.ts
+++ lastService2/src/controller/UserController.ts
@@ -31,6 +31,81 @@ export class UserController {
 
 
 
+
+    async identityInformationOfUser(phoneNumber : string ,birthDate : string ,nationalCode : string){
+        let identityInfoUrl = process.env.IDENTITY_INFO_URL 
+        let shahkarToken = await this.getToken()
+        if (shahkarToken == null || shahkarToken == undefined) {
+            return null
+        }else{
+        let body = {birthDate : birthDate , nationalCode : nationalCode}   
+        try {
+           let res = await axios.post(identityInfoUrl , body , {headers : { 'Authorization' : shahkarToken }})
+            let info  = res.data 
+            console.log('trach code . . .',res.headers['track-code'])
+            console.log('shahkar info>>>>' , res)
+            if(res.status == 200){
+                let  {
+                    firstName,
+                    lastName,
+                    gender,
+                    liveStatus,
+                    identificationNo,
+                    fatherName,
+                    identificationSerial,
+                    identificationSeri,
+                    officeName,
+                  } = info
+                let user = {
+                    fatherName,
+                    gender:(gender == 0) ? false : true
+                    ,officeName,
+                    birthDate,
+                    identityNumber : identificationNo,
+                    identitySeri : identificationSeri,
+                    identitySerial: identificationSerial,
+                    firstName,
+                    lastName,
+                    phoneNumber,
+                    nationalCode,
+                    liveStatus,
+                    verificationStatus : 1,
+                    identityTraceCode : res.headers['track-code'],
+                }
+                const trackObj : trackIdInterface = {
+                    phoneNumber:user.phoneNumber,
+                    trackId:user.identityTraceCode,
+                    fatherName:user.fatherName,
+                    firstName:user.fatherName,
+                    lastName:user.lastName,
+                    status:true
+                }
+                let saveData=new internalDB()
+                const DBStatus=await saveData.saveData(trackObj)
+                console.log('returned db status>>>>' , DBStatus)
+                return  user 
+
+            }else {
+                const trackObj : trackIdInterface = {
+                    phoneNumber:phoneNumber,
+                    trackId:res.headers['track-code'],
+                    status:false
+                }
+                let saveData=new internalDB()
+                await saveData.saveData(trackObj)
+                const DBStatus=await saveData.saveData(trackObj)
+                console.log('returned db status>>>>' , DBStatus)
+                return null          
+            }
+        } catch (error) {
+            console.log(error);
+            return null
+        }     
+        }
+    }
+
+
+
     async getToken() {
         let authUrl = process.env.AUTH_URL
         try {
@@ -201,8 +276,8 @@ export class UserController {
                 return res.status(400).json({ msg: 'شماره تلفن با شماره ملی مطابقت ندارد' })
             }
             console.log("after Is Match");
-
-            const userInfo = await this.shakarService.identityInformationOfUser(phoneNumber, birthDate, nationalCode)
+            console.log(phoneNumber)
+            const userInfo = await this.identityInformationOfUser(phoneNumber, birthDate, nationalCode)
             if (!userInfo) {
                 return next(new responseModel(req, res,'', 'approve new user', 400, " مشکلی در استعلام اطلاعات کاربر رخ داده است لطفا از درست بودن اطلاعات اطمینان حاصل کنید", null))
             }
