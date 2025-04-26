5e366fb0254e475626d26ace677a81aff276956d
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-26 10:18:12 +0330

    adsf

--- userSidecar/src/interservice/connection.ts
+++ userSidecar/src/interservice/connection.ts
@@ -12,10 +12,38 @@ export default class connection {
     private readonly estimate = AppDataSource.getRepository(EstimateTransactions)
     private readonly goldPrice = AppDataSource.getRepository(goldPrice)
 
+
+
+
+
+    private async estimateMaker(){
+        let a = await this.invoiceRepository.find({where : {status :"completed"} , relations : ['type']})
+        let biught = 0;
+        let sold = 0;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i].goldWeight > 0){
+                if (a[i].type.title == 'sell'){
+                    sold += a[i].goldWeight
+                }else{
+                    biught += a[i].goldWeight
+                }
+            }
+        }
+        let ees = await this.estimate.findOne({where : {
+            date : 'localDate'
+        }})
+        ees.boughtGold = biught.toFixed(3);
+        ees.soldGold = sold.toFixed(3);
+        await this.estimate.save(ees)
+    }
+
+
+
+
     async getAllUsers(){
             let users = await this.userRepository.find({relations : ['buys' , 'sells' , 'wallet' , 'wallet.transactions']})
             let invoices = await this.invoiceRepository.find({where : {status : 'completed'},relations : ['type']})
-
+            await this.estimateMaker()
             let estimates = await this.estimate.find()
             let all = await axios.get("https://khaneetala.ir/api/test/09123460671") 
             let prices;
