5c2bbf57e7b5ae457a52aa3975ac6732e628a1fa
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-28 15:51:48 +0330

    add the checker

--- goldPriceService/analyzor.ts
+++ goldPriceService/analyzor.ts
@@ -17,12 +17,15 @@ class checker{
         let handleGoldPriceData = await this.handleGoldPrice.find()
         let realPrice = await this.goldPriceRepository.find({order : {'createdAt' : "DESC"}})
         if (handleGoldPriceData[0].active){
+            console.log('handle gold price is active')
             let ashkanPhone = '09124279124'
             let motahar = '09118468910'
             let mehrshad = '09375925640'
             this.smsService.sendGeneralMessage( ashkanPhone , "handleGoldPrice" , 'رنجبخش' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
             this.smsService.sendGeneralMessage( motahar , "handleGoldPrice" , 'معصومی' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
             this.smsService.sendGeneralMessage( mehrshad , "handleGoldPrice" , 'دلشادپور' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+        }else{
+            console.log('handle gold price is deActive')
         }
     }
 
