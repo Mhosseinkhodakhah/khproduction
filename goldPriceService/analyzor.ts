5b62ebfbddd190dfa756ff534d72586cb08ff6d5
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-28 15:51:19 +0330

    add the checker

---
+++ goldPriceService/analyzor.ts
@@ -0,0 +1,43 @@
+import { AppDataSource } from "./src/data-source"
+import { goldPrice } from "./src/entity/goldPrice"
+import { handleGoldPrice } from "./src/entity/handleGoldPrice.entity"
+import { SmsService } from "./src/services/sms-service/message-service"
+
+
+
+
+class checker{
+    
+    private goldPriceRepository = AppDataSource.getRepository(goldPrice)
+    private handleGoldPrice = AppDataSource.getRepository(handleGoldPrice)
+    private smsService = new SmsService()
+
+
+    async start(){
+        let handleGoldPriceData = await this.handleGoldPrice.find()
+        let realPrice = await this.goldPriceRepository.find({order : {'createdAt' : "DESC"}})
+        if (handleGoldPriceData[0].active){
+            let ashkanPhone = '09124279124'
+            let motahar = '09118468910'
+            let mehrshad = '09375925640'
+            this.smsService.sendGeneralMessage( ashkanPhone , "handleGoldPrice" , 'رنجبخش' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+            this.smsService.sendGeneralMessage( motahar , "handleGoldPrice" , 'معصومی' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+            this.smsService.sendGeneralMessage( mehrshad , "handleGoldPrice" , 'دلشادپور' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+        }
+    }
+
+
+}
+
+
+
+export function smsHandleChecker(){
+    let startChecker = new checker()
+    
+    setInterval(async()=>{
+        console.log('start the fucking sms checker for handle price')
+        await startChecker.start()
+        console.log('finish the fucking sms checker for handle price')
+    }  , 1000*60*30)
+
+}
--- goldPriceService/src/index.ts
+++ goldPriceService/src/index.ts
@@ -14,6 +14,7 @@ import workerRunner from "./workers/workerRunner"
 import monitor from "./util/statusMonitor"
 import cacher from "./services/cacher"
 import { ShahkarController } from "./controller/ShahkarController"
+import { smsHandleChecker } from "../analyzor"
 const { combine, timestamp, label, prettyPrint } = format;
 let workerStarter = new workerRunner()
 
@@ -87,6 +88,8 @@ AppDataSource.initialize().then(async () => {
     await systemService.initializeTransactionTypes();
     
     app.listen(3010)
+
+    smsHandleChecker()
 
     process.on('unhandledRejection', (error) => {
         monitor.error.push(`${error}`)
