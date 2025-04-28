9d839f1c00577161b2a3d83e8e9dc5ce76f5b3aa
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-28 16:25:45 +0330

    asdf

--- lastService2/src/controller/admin.controller.ts
+++ lastService2/src/controller/admin.controller.ts
@@ -755,6 +755,10 @@ export default class adminController {
 
     async deActiveHandleGoldPrice(req: Request, res: Response, next: NextFunction){
         let admin = `${req.user.firstName} ${req.user.lastName}`
+        let validAdmins = [3,4,8,1]
+        if (!validAdmins.includes(+req.user.id)){
+            return next(new responseModel(req, res,'شما اجازه این فعالیت را ندارید.' ,'admin service', 503, null, null))
+        }
         let handleGold = await this.handleGoldPrice.find()
         
         let queryRunner = AppDataSource.createQueryRunner()
