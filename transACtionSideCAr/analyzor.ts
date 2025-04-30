f1a91ed0cbaebd657dc279c963569c353708e280
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-30 12:49:46 +0330

    adsf

--- transACtionSideCAr/analyzor.ts
+++ transACtionSideCAr/analyzor.ts
@@ -44,7 +44,7 @@ class checkTransActions{
         console.log("today",today);
         
         let transactionsToday =await this.invoice.createQueryBuilder('invoice')
-        .where('invoice.tradeType = :bool AND invoice.createdAt <= :today' , {bool : TradeType.ONLINE , today : today})
+        .where('invoice.tradeType = :bool AND status = :status AND invoice.createdAt <= :today' , {bool : TradeType.ONLINE , status : 'init' ,today : today})
         .getMany()
 
         console.log('len of inits' , transactionsToday.length)
