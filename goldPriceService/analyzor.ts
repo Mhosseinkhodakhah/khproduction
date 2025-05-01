c5c16203d14bcbc59f9cb456c85a1610317d2bc5
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-01 23:18:25 +0330

    asdf

--- goldPriceService/analyzor.ts
+++ goldPriceService/analyzor.ts
@@ -12,19 +12,29 @@ class checker{
     private handleGoldPrice = AppDataSource.getRepository(handleGoldPrice)
     private smsService = new SmsService()
     
+    inProcess = false;
+
+
     async start(){
-        let handleGoldPriceData = await this.handleGoldPrice.find()
-        let realPrice = await this.goldPriceRepository.find({order : {'createdAt' : "DESC"}})
-        if (handleGoldPriceData[0].active){
-            console.log('handle gold price is active')
-            let ashkanPhone = '09124279124'
-            let motahar = '09118468910'
-            let mehrshad = '09375925640'
-            this.smsService.sendGeneralMessage( ashkanPhone , "handleGoldPrice" , 'رنجبخش' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
-            this.smsService.sendGeneralMessage( motahar , "handleGoldPrice" , 'معصومی' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
-            this.smsService.sendGeneralMessage( mehrshad , "handleGoldPrice" , 'دلشادپور' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
-        }else{
-            console.log('handle gold price is deActive')
+        this.inProcess = true;
+        try {
+            let handleGoldPriceData = await this.handleGoldPrice.find()
+            let realPrice = await this.goldPriceRepository.find({order : {'createdAt' : "DESC"}})
+            if (handleGoldPriceData[0].active){
+                console.log('handle gold price is active')
+                let ashkanPhone = '09124279124'
+                let motahar = '09118468910'
+                let mehrshad = '09375925640'
+                this.smsService.sendGeneralMessage( ashkanPhone , "handleGoldPrice" , 'رنجبخش' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+                this.smsService.sendGeneralMessage( motahar , "handleGoldPrice" , 'معصومی' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+                this.smsService.sendGeneralMessage( mehrshad , "handleGoldPrice" , 'دلشادپور' ,handleGoldPriceData[0].price , realPrice[0].Geram18)
+            }else{
+                console.log('handle gold price is deActive')
+            }    
+        } catch (error) {
+            console.log('error occured in checking handle goldPrice >> ' , error)
+        }finally{
+            this.inProcess = false
         }
     }
 }
@@ -33,11 +43,13 @@ let startChecker = new checker()
 
 export function smsHandleChecker() {
     try {
-        console.log('sms checker is started >>>>')
         setInterval(() => {
-            console.log('start the fucking sms checker for handle price')
-            startChecker.start()
-            console.log('finish the fucking sms checker for handle price')
+            if (!startChecker.inProcess){
+                console.log('checking handle goldPrice is false > ')
+                startChecker.start()
+            }else {
+                console.log('checking handle goldPrice is true>')
+            }
         }, 1000*60*30)
     } catch (error) {
         console.log('errorrr' , error)
