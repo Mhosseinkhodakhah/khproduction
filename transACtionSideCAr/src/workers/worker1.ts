8bc1773d131a2cd162ee64f90ccbee8ac21f3af2
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-03 00:09:51 +0330

    adf

--- transACtionSideCAr/src/workers/worker1.ts
+++ transACtionSideCAr/src/workers/worker1.ts
@@ -1,20 +1,28 @@
 const {parentPort ,workerData , isMainThread , threadId} =require('worker_threads')
 
 
+function start(){
+    setInterval(()=>{
+        parentPort?.postMessage(
+            `worker1 runed , ${isMainThread} , ${threadId}`
+        );
+    } , 1000)
+}
+
 parentPort.on('message', (data) => {
     console.log('data recieved from main thread >>> ' , data)
     if (data == 'first task'){
         setInterval(()=>{
-            console.log('first task is runnign')
+            console.log('first task is runnign' , `${isMainThread} , ${threadId}`)
         } , 1000)
     }else if (data == 'second task'){
         setInterval(()=>{
-            console.log('second task is runnign')
+            console.log('second task is runnign' , `${isMainThread} , ${threadId}`)
         } , 1000)        
     }
     else if (data == 'thirdTask task'){
         setInterval(()=>{
-            console.log('third task is runnign')
+            console.log('third task is runnign' , `${isMainThread} , ${threadId}`)
         } , 1000)        
     }
     // if (data.task === 'calculate') {
@@ -24,3 +32,4 @@ parentPort.on('message', (data) => {
     // }
 });
 
+start()
