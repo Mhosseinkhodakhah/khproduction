b6ac5e595b82f018975488d8c41d6f237502d460
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-03 00:04:46 +0330

    asdf

--- transACtionSideCAr/src/workers/worker1.ts
+++ transACtionSideCAr/src/workers/worker1.ts
@@ -9,15 +9,15 @@ function start(){
         );
     } , 1000)
     
-    parentPort.on('message', (data) => {
-        console.log('data recieved from main thread >>> ' , data)
-        // if (data.task === 'calculate') {
-        //   const result = data.number * 2; // Simple calculation
-        //   // Send the result back to the main thread
-        //   parentPort.postMessage({ result });
-        // }
-    });
 }
 
+parentPort.on('message', (data) => {
+    console.log('data recieved from main thread >>> ' , data)
+    // if (data.task === 'calculate') {
+    //   const result = data.number * 2; // Simple calculation
+    //   // Send the result back to the main thread
+    //   parentPort.postMessage({ result });
+    // }
+});
 
 start()
