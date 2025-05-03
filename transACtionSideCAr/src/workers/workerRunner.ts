ce2ff303bb5b66b97bda4d5cbbeb68e9ae7e0af7
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-03 11:37:11 +0330

    adsf

--- transACtionSideCAr/src/workers/workerRunner.ts
+++ transACtionSideCAr/src/workers/workerRunner.ts
@@ -4,11 +4,11 @@ import { Worker , isMainThread , threadId } from "worker_threads"
 
 
 export class runTheWorkers{
-    private worker1 = new Worker('./src/workers/worker1.js',  {})
+    private worker1 = new Worker('./workers/worker1.js',  {})
     
-    private worker2 = new Worker('./src/workers/worker1.js', {})
+    private worker2 = new Worker('./workers/worker1.js', {})
 
-    private worker3 = new Worker('./src/workers/worker1.js' , {})
+    private worker3 = new Worker('./workers/worker1.js' , {})
 
     async start(){
         this.worker1.on('message', async(result) => {
