d177c465fbacf941cc34065309fc4878606142bf
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-07 14:59:06 +0330

    adsf

--- branches/src/controller/branch.controller.ts
+++ branches/src/controller/branch.controller.ts
@@ -246,20 +246,19 @@ export default class branchController {
     async getSellers(req: Request, res: Response, next: NextFunction) {
         try {
             let branchId = req.params.branchId;
-            let branch = await this.branchRepository.findOne({ where: { id: +branchId }, relations: ['sellers'] })
-            // let branch = await this.branchRepository.createQueryBuilder('branch')
-            //     .where('branch.id = :id', { id: +branchId })
-            //     .leftJoinAndSelect('branch.sellers', 'sellers')
-            //     .andWhere('sellers.isActive = :isActive', { isActive: true }).orderBy('sellers.code').getOne()
+            // let branch = await this.branchRepository.findOne({ where: { id: +branchId }, relations: ['sellers'] })
+            let branch = await this.branchRepository.createQueryBuilder('branch')
+                .where('branch.id = :id', { id: +branchId })
+                .leftJoinAndSelect('branch.sellers', 'sellers')
+                .andWhere('sellers.isActive = :isActive', { isActive: true }).orderBy('createdAt').getOne()
             if (!branch) {
                 return next(new responseModel(req, res, 'شعبه مورد نظر در سیستم ثبت نشده است', 'branch', 500, 'شعبه مورد نظر در سیستم ثبت نشده است', null))
             }
-            let sellers = await this.sellerRepository.find({where : {branch : branch} , order : {'createdAt' : 'ASC'}})
-            for (let i = 0; i < sellers.length; i++) {
-                let elem = sellers[i]
-                sellers[i].code = `کد ${sellers[i].code}-${sellers[i].firstName[0]}.${sellers[i].lastName}`
+            for (let i = 0; i < branch.sellers.length; i++) {
+                let elem = branch.sellers[i]
+                branch.sellers[i].code = `کد ${branch.sellers[i].code}-${branch.sellers[i].firstName[0]}.${branch.sellers[i].lastName}`
             }
-            return next(new responseModel(req, res, '', 'branch', 200, null, sellers))
+            return next(new responseModel(req, res, '', 'branch', 200, null, branch.sellers))
         } catch (error) {
             console.log('error >>> ', error)
             return next(new responseModel(req, res, 'خطای داخلی سیستم', 'branch', 500, 'خطای داخلی سیستم', null))
