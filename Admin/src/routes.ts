5a4d1e99029f3615b4d1f8baee631fa78880ef50
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-07 12:12:02 +0330

    adsf

--- Admin/src/routes.ts
+++ Admin/src/routes.ts
@@ -11,7 +11,7 @@ export const Routes = [{
     method: "post",
     route: "/menu/create",
     controller: UserController,
-    middleware: [],
+    middleware: [authMiddleware],
     action: "createNewMenu",
 }, {
     method: "post",
