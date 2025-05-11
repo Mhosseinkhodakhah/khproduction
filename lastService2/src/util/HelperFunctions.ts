64556bfc8e02a73e5e9190b8a3e3a64cd23682f1
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-11 15:06:11 +0330

    asdf

--- lastService2/src/util/HelperFunctions.ts
+++ lastService2/src/util/HelperFunctions.ts
@@ -13,6 +13,25 @@
  * @returns 
  */
 export function formatGoldWeight(weight) {
-    return parseFloat((Math.round(weight * 100) / 100).toFixed(3));
+    console.log('its here for validation >>>> ' , weight)
+    let seperator = weight.toString().split('')
+    let mainWeight = ''
+    if (seperator.length == 5) {
+        mainWeight = `${seperator[0]}${seperator[1]}${seperator[2]}${seperator[3]}${seperator[4]}`
+    }else if (seperator.length == 4) {
+        mainWeight = `${seperator[0]}${seperator[1]}${seperator[2]}${seperator[3]}0`
+    } else if (seperator.length == 3) {
+        mainWeight = `${seperator[0]}${seperator[1]}${seperator[2]}00`
+    } else if (seperator.length == 2) {
+        mainWeight = `${seperator[0]}${seperator[1]}000`
+    } else if (seperator.length == 1) {
+        mainWeight = `${seperator[0]}.000`
+    }else if(seperator.length >= 5){
+        mainWeight = `${seperator[0]}${seperator[1]}${seperator[2]}${seperator[3]}${seperator[4]}`
+    }
+    let newGoldWeight = `${mainWeight[0]}${mainWeight[1]}${mainWeight[2]}${mainWeight[3]}${mainWeight[4]}`
+    console.log( 'after validation' , +newGoldWeight )
+    return +newGoldWeight
+    // return parseFloat((Math.round(weight * 100) / 100).toFixed(3));
 }
 
