f9caeb7b38d386b6100f98439d270b63b14b7ed2
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-11 12:09:00 +0330

    adf

--- Admin/src/controller/UserController.ts
+++ Admin/src/controller/UserController.ts
@@ -121,6 +121,7 @@ export class UserController {
 
     async addNewAdmin(req: Request, res: Response, next: NextFunction) {
 
+        console.log(req.body)
         if (!req.body.firstName || !req.body.lasatName || !req.body.phoneNumber || !req.body.password) {
             console.log('its fucking innnnnnn')
             return next(new response(req, res, 'admin', 400, 'مقادیر را وارد کنید', null))
