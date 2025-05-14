ef8152a6d657db0d0c72dd17e8af44bba8ba9337
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-14 15:01:02 +0330

    asdf

--- dataAnalyzor/analyzor/analyze/professionalFilter.py
+++ dataAnalyzor/analyzor/analyze/professionalFilter.py
@@ -195,7 +195,9 @@ class analyzor:
     
 
     
-    
+    ###############################################################
+    ###############################################################
+    ###############################################################
     def walletTransActionMaker(self , data , filter):
         ################## 1 ######################
         cardPan = []
@@ -257,8 +259,122 @@ class analyzor:
         else:
             description = amount
         ################## 6 ######################
+        
+        
+        ################## 7 ######################
+        #filter for accounter
+        startDate = []
+        if (filter['startDate'] != 'all'):
+            mainyear = int(startDate.split('/')[0])
+            mainmonth = int(startDate.split('/')[1])
+            mainday = int(startDate.split('/')[2])
+            for i in description:
+                year = int(i['date'].split('/')[0])
+                month = int(i['date'].split('/')[1])
+                day = int(i['date'].split('/')[2])
+                if (year == mainyear):
+                    print('first condition done >>> ')                    
+                    if (month == mainmonth):
+                        print('second condition done >>> ')                    
+                        if (day >= mainday):
+                            print('third conditions >>> ')
+                            startDate.append(i)
+                    elif(month > mainmonth):
+                        print('forth conditions')
+                        startDate.append(i)
+                elif(year > mainyear):
+                    print('fifth conditions >>>> ')
+                    startDate.append(i)
+        else:
+            startDate = description
+        
+        ################# 7 #######################  
+        
+        
+        
+         ################## 8 ######################
+        endDate=[]
+        if (filter['endDate'] != 'all'):
+            mainyear = int(endDate.split('/')[0])
+            mainmonth = int(endDate.split('/')[1])
+            mainday = int(endDate.split('/')[2])
+            for i in startDate:
+                year = int(i['date'].split('/')[0])
+                month = int(i['date'].split('/')[1])
+                day = int(i['date'].split('/')[2])
+                if (year == mainyear):
+                    if (month == mainmonth):
+                        if (day <= mainday):
+                            endDate.append(i)
+                        
+                    elif(month < mainmonth):
+                        endDate.append(i)
+                elif(year < mainyear):
+                    endDate.append(i)
+        else:
+            endDate = startDate
+        
+        ################## 8 ###################### 
+        
+        
+        ################## 9 ######################
+        startTime = []
+        if (filter['startTime'] != 'all'):
+            mainTime = int(startTime.split(':')[0])
+            for i in endDate:
+                mainyear = int(startDate.split('/')[0])
+                mainmonth = int(startDate.split('/')[1])
+                mainday = int(startDate.split('/')[2])
+                year = int(i['date'].split('/')[0])
+                month = int(i['date'].split('/')[1])
+                day = int(i['date'].split('/')[2])
+                time = int(i['time'].split(':')[0])
+                if (year == mainyear):
+                    if (month == mainmonth):
+                        if (day == mainday):
+                            if (time >= mainTime):
+                                startTime.append(i)
+                        elif (day > mainday):
+                            startTime.append(i)
+                    elif(month > mainmonth):
+                        startTime.append(i)
+                elif(year > mainyear):
+                    startTime.append(i)
+        else:
+            startTime = endDate
+        ################## 9 ######################      
+        
+        
+        
+        
+        ################## 10 ######################
+        endTime = []
+        if (filter['endTime'] != 'all'):
+            mainTime = int(endTime.split(':')[0])
+            for i in startTime:
+                    mainyear = int(endDate.split('/')[0])
+                    mainmonth = int(endDate.split('/')[1])
+                    mainday = int(endDate.split('/')[2])
+                    year = int(i['date'].split('/')[0])
+                    month = int(i['date'].split('/')[1])
+                    day = int(i['date'].split('/')[2])
+                    time = int(i['time'].split(':')[0])
+                    if (year == mainyear):
+                        if (month == mainmonth):
+                            if (day == mainday):
+                                if (time <= mainTime):
+                                    endTime.append(i)
+                            elif (day < mainday):
+                                endTime.append(i)
+                        elif(month < mainmonth):
+                            endTime.append(i)
+                    elif(year < mainyear):
+                        endTime.append(i)
+        else:
+            endTime = startTime
+        ################## 10 ######################
 
-        return description
+        return endTime
         
         
 
@@ -317,7 +433,6 @@ class professionalFilter :
             response = requests.get(f'{self.url}?lastName={filter['lastName']}&type={filter['type']}&status={filter['status']}')
         else:
             response = requests.get(f'{self.url}?type={filter['type']}&status={filter['status']}')
-        
         data = response.json()
         # print(len(data['data']))
         # print(filter)
